6fd890939b753f45e29cc53c0a6e3758f7fccd4c
Sambit Tripathy <sambittripathy200@gmail.com>
2024-08-01 18:35:22 +0530

    Minor changes

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,14 @@
 import React from "react";
+
 const Card = ({ image, date, course, price, duration, max }) => {
   return (
-    <div className="flex flex-col  p-5 gap-5 m-5 w-[18rem] h-[27rem] rounded-xl shadow-lg  duration-500 border-2 border-indigo-700 leading-loose">
+    <div className="mb-8 transition-transform hover:scale-110 flex flex-col  p-5 gap-5 m-5 w-[18rem] h-[27rem] rounded-xl shadow-lg  duration-500 border-2 border-indigo-700 leading-loose">
       <div>
         {/* image section  */}
         <img
           src={image}
           alt="Sanjay Kandula"
-          className="transition-transform hover:scale-110 h-[10rem]"
+          className=" h-[10rem]"
         />
       </div>
 
@@ -30,7 +31,7 @@ const Card = ({ image, date, course, price, duration, max }) => {
 
       <div className="flex  justify-between">
         <h1 className="text-orange-400 font-bold text-xl">{price}</h1>
-        <button className="transition-transform duration-700 w-[10rem] ease-in-out hover:scale-110 bg-orange-400 hover:bg-white border hover:border-orange-500 rounded-full">
+        <button className="w-[10rem] ease-in-out hover:scale-110 bg-orange-400 hover:bg-white border hover:border-orange-500 rounded-full">
           Contact Us
         </button>
       </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,55 +1,17 @@
 import React from "react";
 import Header from "./Header";
 import Aboutus from "./Aboutus";
-import Card from "./Card";
-import image1 from "../assets/web-design.svg";
-import image2 from "../assets/app-development.svg";
-
-
 import Banner from './Banner'
-
 import Footer from './Footer'
+import Courses from "./Courses";
 
 const Home = () => {
   return (
     <div>
-      <Header />
+      <Header/>
        <Banner/>
-     <Aboutus/>
-      <div className="flex flex-wrap justify-start items-center">
-        <Card
-          image={image1}
-          date="August 1, 2024"
-          duration="3 Months"
-          max="30 Students Max"
-          course="React Basics"
-          price="$299"
-        />
-        <Card
-          image={image2}
-          date="August 5, 2024"
-          duration="6 Months"
-          max="20 Students Max"
-          course="Advanced JavaScript"
-          price="$499"
-        />
-        <Card
-          image={image2}
-          date="August 5, 2024"
-          duration="6 Months"
-          max="20 Students Max"
-          course="Advanced JavaScript"
-          price="$499"
-        />
-        <Card
-          image={image2}
-          date="August 5, 2024"
-          duration="6 Months"
-          max="20 Students Max"
-          course="Advanced JavaScript"
-          price="$499"
-        />
-      </div>
+      <Aboutus/>
+      <Courses/>
       <Footer />
     </div>
   );
